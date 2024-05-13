8a43620ab6a5b5bffb03dbb6c33dd4ada7c6e0b2
Seantheman01 <sean.v.g@outlook.com>
2024-05-13 13:43:43 +0200

    spaces added

--- Javascript/Fibonacci/fibonacci.js
+++ Javascript/Fibonacci/fibonacci.js
@@ -1,14 +1,14 @@
 function fibonacci(n) {
     let n1 = 0, n2 = 1, n3;
-    console.log(n1);
-    console.log(n2);
+    document.write(n1 + " ");
+    document.write(n2 + " ");
 
     for (let i = 2; i < n; i++) {
         n3 = n1 + n2;
-        console.log(n3);
+        document.write(n3 + " ");
         n1 = n2;
         n2 = n3;
     }
 }
 
-document.getElementById("printFibonacci").innerHTML = fibonacci(20);
+fibonacci(20);
