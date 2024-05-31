4e461fb952697e2bf4d0741fa3963ae68c5f639e
Seantheman01 <sean.v.g@outlook.com>
2024-05-31 15:42:02 +0200

    revesred days

--- Javascript/Dagen/dagen.js
+++ Javascript/Dagen/dagen.js
@@ -8,6 +8,16 @@ for (let y of Array(5).keys()) {
     document.getElementById("werkDagen").innerHTML += dagen[y] + "<br>";
 }
 
-for (let z of Array().keys()) {
-    document.getElementById("weekendDagen").innerHTML += dagen[z] + "<br>";
+const weekend = dagen.slice(-2);
+for (let z of weekend) {
+    document.getElementById("weekendDagen").innerHTML += z + "<br>";
+}
+
+const andersom = dagen.reverse();
+for (let a in andersom) {
+    document.getElementById("alleDagenAndersom").innerHTML += dagen[a] + "<br>";
+}
+
+for (let b of Array(5).keys()) {
+    document.getElementById("werkDagen").innerHTML += dagen[b] + "<br>";
 }
