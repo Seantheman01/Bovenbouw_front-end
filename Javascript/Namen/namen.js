e7956bd308e475d9d6fd91e4426bccc3c1e3498c
Seantheman01 <sean.v.g@outlook.com>
2024-06-13 21:30:38 +0200

    empty array added

--- Javascript/Namen/namen.js
+++ Javascript/Namen/namen.js
@@ -1,4 +1,5 @@
-let namen = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)")
+let namenLijst = [];
+let namen = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
 
 if (namen != "") {
     console.log("Het werkt");
