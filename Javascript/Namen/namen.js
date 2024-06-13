625b801c013396a2a3b074fb8d065d587bdf3735
Seantheman01 <sean.v.g@outlook.com>
2024-06-13 21:52:19 +0200

    fixed the loop

--- Javascript/Namen/namen.js
+++ Javascript/Namen/namen.js
@@ -1,6 +1,16 @@
-let namenLijst = [];
-let namen = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
+while (true) {
+    let namenLijst = [];
+    let namenAantal = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
 
-if (namen != "") {
-    console.log("Het werkt");
+    if (namenAantal >= 3) {
+        for (let i = 0; i < namenAantal; i++) {
+            let namen = prompt("Voer een naam in: ");
+            namenLijst.push(namen);
+        }
+        window.alert("De ingevoerde namen zijn: " + namenLijst);
+        break;
+
+    } else {
+        window.alert("U moet minimaal 3 namen invoeren.");
+    }
 }
