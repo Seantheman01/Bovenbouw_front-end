ea95566e7d9467b04210cc3d4937d31dfd50476a
Seantheman01 <sean.v.g@outlook.com>
2024-06-14 15:30:14 +0200

    added multiply and divide

--- Javascript/Rekenen/rekenen.js
+++ Javascript/Rekenen/rekenen.js
@@ -2,9 +2,9 @@ var arrayEen = [1,2,3,4,5,6,7,8,9,10];
 var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
 
 function optellen(arrayEen, arrayTwee) {
-    for (let x = 0; x < arrayEen.length; x++) {
-        let som1 = arrayEen[x] + arrayTwee[x];
-        document.write(arrayEen[x] + " + " + arrayTwee[x] + " = " + som1 + "<br>");
+    for (let i= 0; i < arrayEen.length; i++) {
+        let som = arrayEen[i] + arrayTwee[i];
+        document.write(arrayEen[i] + " + " + arrayTwee[i] + " = " + som + "<br>");
     }
 }
 document.write("<h2>Optellen van de twee arrays zijn:</h2>");
@@ -12,10 +12,30 @@ optellen(arrayEen, arrayTwee);
 
 
 function aftrekken(arrayTwee, arrayEen) {
-    for (let y = 0; y < arrayEen.length; y++) {
-        let som2 = arrayTwee[y] - arrayEen[y];
-        document.write(arrayTwee[y] + " - " + arrayEen[y] + " = " + som2 + "<br>");
+    for (let i = 0; i < arrayEen.length; i++) {
+        let som = arrayTwee[i] - arrayEen[i];
+        document.write(arrayTwee[i] + " - " + arrayEen[i] + " = " + som + "<br>");
     }
 }
 document.write("<h2>Aftrekken van de twee arrays zijn:</h2>");
 aftrekken(arrayTwee, arrayEen);
+
+
+function vermenigvuldigen(arrayEen, arrayTwee) {
+    for (let i = 0; i < arrayEen.length; i++) {
+        let som = arrayEen[i] * arrayTwee[i];
+        document.write(arrayEen[i] + " x " + arrayTwee[i] + " = " + som + "<br>");
+    }
+}
+document.write("<h2>Aftrekken van de twee arrays zijn:</h2>");
+vermenigvuldigen(arrayTwee, arrayEen);
+
+
+function delen(arrayTwee, arrayEen) {
+    for (let i = 0; i < arrayEen.length; i++) {
+        let som = arrayTwee[i] / arrayEen[i];
+        document.write(arrayTwee[i] + " : " + arrayEen[i] + " = " + som + "<br>");
+    }
+}
+document.write("<h2>Aftrekken van de twee arrays zijn:</h2>");
+delen(arrayTwee, arrayEen);
