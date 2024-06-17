1604593454477dba022b193aa19ea37968a2d396
Seantheman01 <sean.v.g@outlook.com>
2024-06-17 13:39:45 +0200

    piramid formed

---
+++ Javascript/Piramide/piramide.js
@@ -0,0 +1,12 @@
+getallen = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+document.write("Array: " + getallen + "<br>");
+
+for (let i = 0; i < getallen.length; i++){
+    for (let x = 0; x <= i; x++){
+        document.write(x);
+    }
+    piramide = getallen.slice(0,0);
+    piramide += "<br>";
+    document.write(piramide);
+}
