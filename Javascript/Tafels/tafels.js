fa0e52487f6d06134229f85654eddd10e759681c
Seantheman01 <sean.v.g@outlook.com>
2024-06-17 10:27:53 +0200

    4 functions

--- Javascript/Tafels/tafels.js
+++ Javascript/Tafels/tafels.js
@@ -1,9 +1,40 @@
 var tafels = [2,4,6,8];
 
-function tafelSommen(tafels) {
+function tafelSommen1(tafels) {
     for (let i = 1; i < 11; i++) {
-        console.log(tafels[0] + " x " + i + " = " + tafels[0] * i);
+        document.write(tafels[0] + " x " + i + " = " + tafels[0] * i + "<br>");
     }
 }
 
-tafelSommen(tafels);
+document.write("<h2>Tafel van 2:</h2>")
+tafelSommen1(tafels);
+
+
+function tafelSommen2(tafels) {
+    for (let i = 1; i < 11; i++) {
+        document.write(tafels[1] + " x " + i + " = " + tafels[1] * i + "<br>");
+    }
+}
+
+document.write("<h2>Tafel van 4:</h2>")
+tafelSommen2(tafels);
+
+
+function tafelSommen3(tafels) {
+    for (let i = 1; i < 11; i++) {
+        document.write(tafels[2] + " x " + i + " = " + tafels[2] * i + "<br>");
+    }
+}
+
+document.write("<h2>Tafel van 6:</h2>")
+tafelSommen3(tafels);
+
+
+function tafelSommen4(tafels) {
+    for (let i = 1; i < 11; i++) {
+        document.write(tafels[3] + " x " + i + " = " + tafels[3] * i + "<br>");
+    }
+}
+
+document.write("<h2>Tafel van 8:</h2>")
+tafelSommen4(tafels);
