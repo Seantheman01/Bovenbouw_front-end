8142a10376ab4d524356c898805dc833053c7e21
Seantheman01 <sean.v.g@outlook.com>
2024-06-21 10:39:08 +0200

    fixed the code

--- Javascript/Piramide_Af/piramide_af.js
+++ Javascript/Piramide_Af/piramide_af.js
@@ -1,5 +1,10 @@
 let getallen = parseInt(prompt("Voer een getal in: "));
-let array = getallen.toString();
+let array = [];
+
+for (let i = 0; i <= getallen; i++) {
+    array.push(i);
+}
+document.write("Array: " + array.join(",") + "<br>");
 
 for (let i = getallen; i >= 0; i--) {
     for (let x = 0; x <= i; x++) {
