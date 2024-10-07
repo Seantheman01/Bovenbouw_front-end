075a506965817fb642212acb423b9e45ab942a25
Seantheman01 <sean.v.g@outlook.com>
2024-10-07 11:19:08 +0200

    start made

---
+++ Javascript/Horeca_App/horeca_app.js
@@ -0,0 +1,7 @@
+document.write("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.");
+
+function aantal_bolletjes() {
+    while (true) {
+        let aantal = prompt("Hoeveel bolletjes wilt u?");
+    }
+}
