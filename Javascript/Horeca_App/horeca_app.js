741d8ae238442def69bd4caff22ff5f71b1a0a8e
Seantheman01 <sean.v.g@outlook.com>
2024-10-07 11:50:27 +0200

    more stuff added

--- Javascript/Horeca_App/horeca_app.js
+++ Javascript/Horeca_App/horeca_app.js
@@ -1,7 +1,16 @@
-document.write("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.");
+alert("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.");
 
 function aantal_bolletjes() {
     while (true) {
-        let aantal = prompt("Hoeveel bolletjes wilt u?");
+        let aantal = parseInt(prompt("Hoeveel bolletjes wilt u?"));
+        if (aantal >= 1 && aantal <= 3) {
+            return aantal;
+        }
+        else if (aantal >= 4 && aantal <= 8) {
+            alert("Dan krijgt u van mij een bakje met " + aantal + " bolletjes.");
+            return aantal;
+        }
     }
 }
+
+aantal_bolletjes();
