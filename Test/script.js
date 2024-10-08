c3c72e76d891f8e1a9d03b48d7b856d781871f22
Seantheman01 <sean.v.g@outlook.com>
2024-10-08 14:07:30 +0200

    buttons give alert

--- Test/script.js
+++ Test/script.js
@@ -4,7 +4,15 @@ let container = document.getElementById("namenContainer");
 for (let i = 0; i < namen.length; i++) {
     console.log(namen[i]);
     var button = document.createElement("button");
+
     button.innerText = namen[i];
+    button.addEventListener("click", function() {
+        buttonClicked(namen[i]);
+    });
 
     container.appendChild(button);
+}
+
+function buttonClicked(name) {
+    alert(name + " button clicked");
 }
