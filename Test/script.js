b95161a217196816556bb0f08afb4bf7e5469f4d
Seantheman01 <sean.v.g@outlook.com>
2024-10-08 13:57:49 +0200

    buttons added

--- Test/script.js
+++ Test/script.js
@@ -1,5 +1,10 @@
 var namen = ['Oğulcan', 'Dinho', 'Meli', 'Kahlid', 'Sean', 'Yassine', 'Bent', "Aydin"];
+let container = document.getElementById("namenContainer");
 
 for (let i = 0; i < namen.length; i++) {
     console.log(namen[i]);
+    var button = document.createElement("button");
+    button.innerText = namen[i];
+
+    container.appendChild(button);
 }
