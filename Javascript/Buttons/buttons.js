38af23165eb7bca8a94098d38b885d800fe7957f
Seantheman01 <sean.v.g@outlook.com>
2024-10-14 14:09:57 +0200

    2 more buttons added

--- Javascript/Buttons/buttons.js
+++ Javascript/Buttons/buttons.js
@@ -1,4 +1,11 @@
-const button = document.createElement('button');
-button.textContent = 'Click me';
+button = document.createElement("button");
+button.textContent = "Button 1";
+container.appendChild(button);
 
+button = document.createElement("button");
+button.textContent = "Button 2";
+container.appendChild(button);
+
+button = document.createElement("button");
+button.textContent = "Button 3";
 container.appendChild(button);
