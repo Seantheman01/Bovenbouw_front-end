ed1c20499ac6bf991433a2f6f09cc4f8b61bf1cf
Seantheman01 <sean.v.g@outlook.com>
2024-10-21 01:03:49 +0200

    for loop added

--- Javascript/Groen_en_rood/g&r.js
+++ Javascript/Groen_en_rood/g&r.js
@@ -1,2 +1,6 @@
-button = document.createElement("button");
+aantal_buttons = parseInt(prompt("Hoeveel knoppen wil je?"));
 
+for (let i = 0; i < aantal_buttons; i++) {
+    button = document.createElement("button");
+    container.appendChild(button);
+}
