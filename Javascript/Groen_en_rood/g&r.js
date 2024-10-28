7b9780a6ec209d788d548087d5ad255491d100da
Seantheman01 <sean.v.g@outlook.com>
2024-10-28 23:31:34 +0100

    event listener added

--- Javascript/Groen_en_rood/g&r.js
+++ Javascript/Groen_en_rood/g&r.js
@@ -1,6 +1,10 @@
-aantal_buttons = parseInt(prompt("Hoeveel knoppen wil je?"));
+aantal_buttons = 30;
 
 for (let i = 0; i < aantal_buttons; i++) {
     button = document.createElement("button");
     container.appendChild(button);
+
+    button.addEventListener("click", function() {
+        button.style.backgroundColor = "red";
+    });
 }
