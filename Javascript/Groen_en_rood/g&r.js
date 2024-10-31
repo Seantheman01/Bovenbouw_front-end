16bdd9ad7dac91bfd961a3e7bbc5e448f1a2ff48
Seantheman01 <sean.v.g@outlook.com>
2024-10-31 15:31:21 +0100

    green buttons

--- Javascript/Groen_en_rood/g&r.js
+++ Javascript/Groen_en_rood/g&r.js
@@ -3,6 +3,7 @@ aantal_buttons = 30;
 for (let i = 0; i < aantal_buttons; i++) {
     button = document.createElement("button");
     container.appendChild(button);
+    button.style.backgroundColor = "green";
 
     button.addEventListener("click", function() {
         button.style.backgroundColor = "red";
