3b75bdeb8fef1d21311e763f101c675c3b08e031
Seantheman01 <sean.v.g@outlook.com>
2024-11-01 17:02:32 +0100

    button changes color

--- Javascript/Kleuren/kleuren.js
+++ Javascript/Kleuren/kleuren.js
@@ -1,4 +1,4 @@
-aantal_buttons = 30;
+let aantal_buttons = 30;
 
 for (let i = 0; i < aantal_buttons; i++) {
     let button = document.createElement("button");
@@ -6,7 +6,21 @@ for (let i = 0; i < aantal_buttons; i++) {
     container.appendChild(button);
     button.style.backgroundColor = "green";
 
+    let clickCount = 0;
+
     button.addEventListener("click", function() {
-        button.style.backgroundColor = "red";
+        clickCount++;
+
+        if (clickCount === 1) {
+            button.style.backgroundColor = "red";
+        } else if (clickCount === 2) {
+            button.style.backgroundColor = "purple";
+        } else if (clickCount === 3) {
+            button.style.backgroundColor = "blue";
+        } else if (clickCount === 4) {
+            button.style.backgroundColor = "black";
+        } else if (clickCount === 5) {
+            button.remove();
+        }
     });
 }
