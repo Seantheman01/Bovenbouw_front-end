a567901db4bfe1b3e8ffb2e66a8e2ee062d1ccd7
Seantheman01 <sean.v.g@outlook.com>
2024-11-02 15:29:32 +0100

    user can choose colors

--- Javascript/Kleuren/kleuren.js
+++ Javascript/Kleuren/kleuren.js
@@ -1,5 +1,9 @@
 let aantal_buttons = 30;
 
+let kleur1 = prompt("Welke kleur word de eerste knop? (in Engels)");
+let kleur2 = prompt("Welke kleur word de tweede knop? (in Engels)");
+let kleur3 = prompt("Welke kleur word de derde knop? (in Engels)");
+
 for (let i = 0; i < aantal_buttons; i++) {
     let button = document.createElement("button");
     button.innerText = i + 1;
@@ -12,11 +16,11 @@ for (let i = 0; i < aantal_buttons; i++) {
         clickCount++;
 
         if (clickCount === 1) {
-            button.style.backgroundColor = "red";
+            button.style.backgroundColor = kleur1;
         } else if (clickCount === 2) {
-            button.style.backgroundColor = "purple";
+            button.style.backgroundColor = kleur2;
         } else if (clickCount === 3) {
-            button.style.backgroundColor = "blue";
+            button.style.backgroundColor = kleur3;
         } else if (clickCount === 4) {
             button.style.backgroundColor = "black";
         } else if (clickCount === 5) {
