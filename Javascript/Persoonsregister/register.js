75d155bc7282f01f99c5f1d6168cb5c8742a87da
Seantheman01 <sean.v.g@outlook.com>
2024-11-14 13:03:07 +0100

    test

--- Javascript/Persoonsregister/register.js
+++ Javascript/Persoonsregister/register.js
@@ -1,6 +1,6 @@
-import data from './opdracht1_bijlage.json' assert { type: 'json' };
-console.log(data);
+// import data from './opdracht1_bijlage.json' assert { type: 'json' };
+// console.log(data);
 
-// var iets = fetch('opdracht1_bijlage.json')
-// .then(x => x.text())
-// .then(y => document.getElementById("demo").innerHTML = y);
+fetch('opdracht1_bijlage.json')
+.then(x => x.text())
+.then(y => document.getElementById("demo").innerHTML = y);
